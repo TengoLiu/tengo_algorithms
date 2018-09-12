195488b73f90e3dc8cf0f5b6a0416231d4edd3bc
tengo_liu <liu5087120!@163.com>
2018-09-12 13:12:47 +0800

    添加冒泡排序的递归实现

--- Tengo算法2018年8月30日起/BubbleSort.cs
+++ Tengo算法2018年8月30日起/BubbleSort.cs
@@ -30,5 +30,32 @@ namespace Tengo算法2018年8月30日起 {
                 arr[index] = temp;
             }
         }
+
+        //递归实现
+        public void SortByRecusive(int start) {
+            //如果已经排到最后一个元素了的话，说明前面都归位了，这一个元素自然也归位了
+            if(start >= arr.Length - 1) {
+                return;
+            }
+            else {
+                //设置最小值的位置，开始默认为当前要查询的第一个位置
+                var index = start;
+
+                //从第i个元素开始找出应该放到第i个位置的数字
+                for(int j = start; j < arr.Length; j++) {
+                    //如果找到一个比基础值还小的数，那么指针指向该值
+                    if(arr[j] < arr[index]) {
+                        index = j;
+                    }
+                }
+                //最后把刚才找到的最小值和第i个元素换位，这样就实现了一个数的归位
+                var temp = arr[start];
+                arr[start] = arr[index];
+                arr[index] = temp;
+
+                //然后对n+1个元素进行归位操作
+                SortByRecusive(start + 1);
+            }
+        }
     }
 }
